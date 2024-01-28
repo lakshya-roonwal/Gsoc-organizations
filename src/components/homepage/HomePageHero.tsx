6e2a84e29326f086b4edfe488a54d5a494b5b4d9
lakshya-roonwal <erenpool7214@gmail.com>
2024-01-28 17:33:06 +0530

    home page footer

--- src/components/homepage/HomePageHero.tsx
+++ src/components/homepage/HomePageHero.tsx
@@ -1,23 +1,19 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { LucideGithub } from "lucide-react";
 
 const HomePageHero = () => {
   return (
     <div className="h-screen bg-white ">
       <div
         style={{
-          height: "150px",
-          borderRadius: "50%",
-          width: "80%",
-          margin: "0 auto",
           left: "0",
           right: "0",
-          position: "absolute",
           transform: "translateY(-50%)",
           background: "linear-gradient(90deg, #e44141, #4f2af3)",
           filter: "blur(110px)",
         }}
-        className=""
+        className="h-[150px] w-[80%] rounded-full my-0 mx-auto absolute"
       ></div>
       <div className="flex flex-col items-center justify-start h-full py-20 md:py-24 ">
         <h1 className="text-5xl md:text-3xl font-bold text-center tracking-tighter sm:text-5xl xl:text-6xl/none">
@@ -42,6 +38,12 @@ const HomePageHero = () => {
             Explore Organizations
           </Link>
         </Button>
+        <div className="absolute bottom-0 mb-4 flex flex-col items-center">
+          <p>My by <Link target="__blank" href={'https://twitter.com/Lakshya_roonwal'} className="text-blue-500 hover:text-blue-700">@Lakshya_roonwal</Link></p>
+          <Link target="__blank" href={'https://github.com/lakshya-roonwal'}>
+          <LucideGithub />
+          </Link>
+        </div>
       </div>
     </div>
   );
