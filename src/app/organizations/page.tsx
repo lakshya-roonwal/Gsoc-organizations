faa155fcb4928e1d1ca17508a916527331dd12aa
lakshya-roonwal <erenpool7214@gmail.com>
2024-02-01 10:33:29 +0530

    Build error 2

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,13 +1,7 @@
 import Organizations from "../AppPages/Organizations";
 
 async function getData() {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL+"/api/getallorgs",{
-    method: "GET", 
-    cache: "no-store", 
-    headers: {  
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL+"/api/getallorgs");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
